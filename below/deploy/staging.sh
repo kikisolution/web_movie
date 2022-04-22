f4d38fc21c3f2109e0af8a17a74e86da79ccf0a0
Ezequias Fonseca <ezequiasfonseca@gmail.com>
2022-04-22 11:20:37 -0300

    Update Action Deploy

--- below/deploy/staging.sh
+++ below/deploy/staging.sh
@@ -1,6 +1,7 @@
 echo "Deploy staging script started."
 cd /home/ubuntu/data/www/web/staging/movies
-git pull origin master
+touch testkiki.txt
+#git pull origin master
 composer install --no-dev --optimize-autoloader
 exit
 echo "Deply staging finished execution."
