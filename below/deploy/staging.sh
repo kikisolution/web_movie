3d4436c8aa84b87048e08d2f49da184d85123eea
Ezequias Fonseca <ezequiasfonseca@gmail.com>
2022-04-22 13:18:20 -0300

    Update Action Deploy

--- below/deploy/staging.sh
+++ below/deploy/staging.sh
@@ -1,6 +1,6 @@
-echo "Deploy staging script started."
+echo "#---Deploy staging script started."
 cd /home/ubuntu/data/www/web/staging/movies
-echo "Git pull origin master"
+echo "#---Git pull origin master"
 git pull origin master
 composer install --no-dev --optimize-autoloader
 npm install
