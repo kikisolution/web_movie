a647cc529a83767eee58bdc4337721528c763d98
Ezequias Fonseca <ezequiasfonseca@gmail.com>
2022-04-22 13:14:59 -0300

    Update Action Deploy

--- below/deploy/staging.sh
+++ below/deploy/staging.sh
@@ -4,5 +4,6 @@ echo "Git pull origin master"
 git pull origin master
 composer install --no-dev --optimize-autoloader
 npm install
+npm run production
 exit
 echo "Deply staging finished execution."
